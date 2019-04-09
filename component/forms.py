b4da30f66ebd1417acd8a79364e8b5dd65e3683f
boycatbay <patiphan_pinkeaw@outlook.com>
2019-04-09 11:32:18 +0700

    updateSettings

--- component/forms.py
+++ component/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class announces(forms.Form):
     textarea = forms.CharField(label='Textarea',max_length=300)
-    material = forms.URLField(label='Material',max_length=100)
+    material = forms.CharField(label='Material',max_length=100)
 
 
 
