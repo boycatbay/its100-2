3ef3133741e07da15763cbf0909e18cf9da2d413
boycatbay <patiphan_pinkeaw@outlook.com>
2019-04-30 14:08:19 +0700

    updateAll

--- web/settings.py
+++ web/settings.py
@@ -149,8 +149,13 @@ SOCIAL_AUTH_PIPELINE = (
 
 
 STATIC_URL='/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+if not DEBUG: 
+    STATIC_ROOT='/home/its100/its100source/static'
+
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
     "593460008764-eg4u8k027qjoc345oq0c5sq80pqk1vkc.apps.googleusercontent.com"
