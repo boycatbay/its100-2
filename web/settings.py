93f84f281bf67ce1b0a9f6eaa03bafcdd7025a4f
boycatbay <patiphan_pinkeaw@outlook.com>
2019-04-30 13:14:01 +0700

    updateAll

--- web/settings.py
+++ web/settings.py
@@ -57,8 +57,19 @@ ROOT_URLCONF = "web.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                'social_django.context_processors.backends',  # <--
+                'social_django.context_processors.login_redirect', # <--
+            ]
+        },
     }
 ]
 
